c889a412553fcf3d41b78171a120f5a8e7aec6df
thiago-augusto-rodrigues <rodrigues.thiago.88@outlook.com>
2024-03-16 20:39:47 -0300

    Voltando para estado do Paulo

--- app.js
+++ app.js
@@ -4,4 +4,3 @@ console.log("aplicação está se conectando ao banco de dados")
 
 // mais um console log de teste:
 console.log("O Paulo fez esse commit sem você ver na aula!")
-console.log("Enviando para o github")
